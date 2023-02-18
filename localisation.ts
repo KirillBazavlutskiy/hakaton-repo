c42c0e5e16052cf7caed2c902e139bcf7e15e011
Omega Men <vanapasenko886@gmail.com>
2023-02-18 16:02:21 +0200

    bag fixing

---
+++ localisation.ts
@@ -0,0 +1,22 @@
+// @ts-ignore
+const localisation = {
+    firstH1Text:{
+        ua: "Підтримайте українську\n" +
+            "Армію, територіальну оборону\n" +
+            "бійців, біженців,\n" +
+            "лікарнів, притулки для тварин",
+
+        en: "Support the Ukrainian\n" +
+            "Army, territorial defense\n" +
+            "fighters, refugees,\n" +
+            "hospitals, animals shelters\n"
+    },
+    OP:{
+        ua: "Нашi проекти",
+        en: "Our Projects"
+    },
+    OPButtonF: {
+        ua:"",
+        en:"Stay Save Kids"
+    },
+}
