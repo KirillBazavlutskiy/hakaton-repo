f03140cd48c2e43f764148b152137cba7570df96
Иван Пащенко <vanapasenko886@gmail.com>
2023-03-02 17:00:16 +0200

    style fixing

--- src/components/OurProjects/OurProjects.tsx
+++ src/components/OurProjects/OurProjects.tsx
@@ -42,7 +42,7 @@ const OurProjects: FC = () => {
                     </div>
                 </div>
                 <div className={s.scrollBtn}>
-                    <button onClick={() => setScrollPosition(--scrollValue)}>
+                    <button onClick={() => (scrollPosition === 0 ? console.log("We haven't more progects") : setScrollPosition(--scrollValue))}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.8">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
                         </svg>
@@ -53,7 +53,7 @@ const OurProjects: FC = () => {
                         <button className={scrollPosition === 1 ? s.checked : s.notChecked} onClick={() => setScrollPosition(1)}>.</button>
                         <button className={scrollPosition === 2 ? s.checked : s.notChecked} onClick={() => setScrollPosition(2)}>.</button>
                     </div>
-                    <button onClick={() => setScrollPosition(++scrollValue)}>
+                    <button onClick={() => (scrollPosition === 2 ? console.log("We haven't more progects") : setScrollPosition(++scrollValue))}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.8">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
                         </svg>
