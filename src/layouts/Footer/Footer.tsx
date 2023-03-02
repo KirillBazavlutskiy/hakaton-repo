0c5bb37bc3d226a6c095f37d302294ffb8a2425a
sadovenk0 <sadovenk0@icloud.com>
2023-03-02 13:08:59 +0200

    added footer

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,16 +1,43 @@
-import { FC } from 'react';
+import Image from "next/image";
+
+import { useDispatch } from "react-redux";
+import { useAppSelector } from "@/redux/store";
+import { changeLanguage } from "@/redux/Slices/LanguageSlice";
 
 import s from './Footer.module.scss';
 import cn from 'classnames';
 
-interface FooterProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
+import Logo from "../../images/logo-min.png";
+import Telegram from "../../images/telegram.png";
+import Instagram from "../../images/instagram.png";
+
+interface FooterProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> { }
 
 export function Footer({ className, ...props }: FooterProps): JSX.Element {
+    const dispatch = useDispatch();
+    const { language } = useAppSelector(state => state.language);
+
     return (
-        <footer className={cn(s.footer, className)} {...props}>
-            <>
-                <span><span className={s.designed}>designed</span> by F5</span> 
-            </>
+        <footer className={cn(s.wrapper, className)} {...props}>
+            <div className={cn(s.container)}>
+                <div className={s.side}>
+                    <Image src={Logo} alt="logo" />
+                    <a href='#'>Privacy Policy</a>
+                </div>
+                <div className={s.contacts}>
+                    <a href='#'>
+                        <Image src={Telegram} alt="telegram-icon" />
+                    </a>
+                    <a href='#'>
+                        <Image src={Instagram} alt="instagram-icon" />
+                    </a>
+                    <a href='mailto:info@staysafe.in.ua'>info@staysafe.in.ua</a>
+                </div>
+                <div className={s.functionalBlock}>
+                    <button onClick={() => dispatch(changeLanguage(language === 'en' ? 'ua' : 'en'))}>{language}</button>
+                    <a href="#B56">Donate</a>
+                </div>
+            </div>
         </footer>
     );
 };
