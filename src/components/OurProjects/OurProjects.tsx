4acdb33eac1711016cd62ed1e83d5c365cd2eb02
Иван Пащенко <vanapasenko886@gmail.com>
2023-03-02 16:50:35 +0200

    bag fix

--- src/components/OurProjects/OurProjects.tsx
+++ src/components/OurProjects/OurProjects.tsx
@@ -40,23 +40,25 @@ const OurProjects: FC = () => {
                             ))
                         }
                     </div>
-                    <div className={s.scrollBtn}>
-                        <button onClick={() => setScrollPosition(--scrollValue)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"/>
-                            </svg>
-                        </button>
-                        <div className={s.points}>
-                            <button className={scrollPosition === 0 ? s.checked : s.notChecked} onClick={() => setScrollPosition(0)}>.</button>
-                            <button className={scrollPosition === 1 ? s.checked : s.notChecked} onClick={() => setScrollPosition(1)}>.</button>
-                            <button className={scrollPosition === 2 ? s.checked : s.notChecked} onClick={() => setScrollPosition(2)}>.</button>
-                        </div>
-                        <button onClick={() => setScrollPosition(++scrollValue)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"/>
-                            </svg>
-                        </button>
+                </div>
+                <div className={s.scrollBtn}>
+                    <button onClick={() => setScrollPosition(--scrollValue)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.8">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                        </svg>
+
+                    </button>
+                    <div className={s.points}>
+                        <button className={scrollPosition === 0 ? s.checked : s.notChecked} onClick={() => setScrollPosition(0)}>.</button>
+                        <button className={scrollPosition === 1 ? s.checked : s.notChecked} onClick={() => setScrollPosition(1)}>.</button>
+                        <button className={scrollPosition === 2 ? s.checked : s.notChecked} onClick={() => setScrollPosition(2)}>.</button>
                     </div>
+                    <button onClick={() => setScrollPosition(++scrollValue)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.8">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                        </svg>
+
+                    </button>
                 </div>
                 <div className={s.content}>
                     <Swiper
