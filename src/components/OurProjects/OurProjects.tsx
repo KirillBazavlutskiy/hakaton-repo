94422e08fce089b71ed7b96767aecf503fd20f0a
”Danylo <monstr14ekss@gmail.com>
2023-03-25 10:52:40 +0200

    Our Projects: mapped imgs, getting data from db

--- src/components/OurProjects/OurProjects.tsx
+++ src/components/OurProjects/OurProjects.tsx
@@ -17,95 +17,19 @@ const OurProjects: FC = () => {
 
     const [activeTab, setActiveTab] = useState<number>(0);
 
-    const [tabs, setTabs] = useState<IProject[]>([
-        {
-            id: "1",
-            name: "project 1",
-            description_EN: "description_en1",
-            description_UA: "description_ua1",
-            imageUrl: "url"
-        },
-        {
-            id: "2",
-            name: "project 2",
-            description_EN: "description_en2",
-            description_UA: "description_ua2",
-            imageUrl: "url"
-        },
-        {
-            id: "3",
-            name: "project 3",
-            description_EN: "description_en3",
-            description_UA: "description_ua3",
-            imageUrl: "url"
-        },
-        {
-            id: "4",
-            name: "project 4",
-            description_EN: "description_en4",
-            description_UA: "description_ua4",
-            imageUrl: "url"
-        },
-        {
-            id: "5",
-            name: "project 5",
-            description_EN: "description_en5",
-            description_UA: "description_ua5",
-            imageUrl: "url"
-        },
-        {
-            id: "6",
-            name: "project 6",
-            description_EN: "description_en6",
-            description_UA: "description_ua6",
-            imageUrl: "url"
-        },
-        {
-            id: "7",
-            name: "project 7",
-            description_EN: "description_en7",
-            description_UA: "description_ua7",
-            imageUrl: "url"
-        },
-        {
-            id: "8",
-            name: "project 8",
-            description_EN: "description_en8",
-            description_UA: "description_ua8",
-            imageUrl: "url"
-        },
-        {
-            id: "9",
-            name: "project 9",
-            description_EN: "description_en9",
-            description_UA: "description_ua9",
-            imageUrl: "url"
-        },
-        {
-            id: "10",
-            name: "project 10",
-            description_EN: "description_en10",
-            description_UA: "description_ua10",
-            imageUrl: "url"
-        }
-    ]);
+    const [tabs, setTabs] = useState<IProject[]>([{
+        name: "Error of projects preload",
+        description_EN: "Ops! Something went wrong...",
+        description_UA: "Ой! Щось пійшло не так!",
+        imageUrl: "",
+        id: "error",
+    }]);
 
-    // useEffect(() => {
-    //     AdminService.GetProjects()
-    //         .then((responce) => setTabs(responce))
-    //         .catch((error) => console.log(error));
-    // }, [])
-
-    const projectNameArrayGrouping = (): IProject[][] => {
-        const arr: IProject[][] = [];
-        for (let i = 0; i < tabs.length / 3; i++) {
-            arr.push([]);
-            for (let j = i * 3; j < i * 3 + 3; j++) {
-                arr[i].push(tabs[j]);
-            }
-        }
-        return arr;
-    }
+    useEffect(() => {
+        AdminService.GetProjects()
+            .then((response) => { setTabs(response); console.log(response) })
+            .catch((error) => console.log(error));
+    }, [])
 
     const projectNamesMapping = () => {
         const arr: IProject[][] = projectNameArrayGrouping();
@@ -121,6 +45,17 @@ const OurProjects: FC = () => {
                 </SwiperSlide>)
             )
         )
+    }
+
+    const projectNameArrayGrouping = (): IProject[][] => {
+        const arr: IProject[][] = [];
+        for (let i = 0; i < tabs.length / 3; i++) {
+            arr.push([]);
+            for (let j = i * 3; j < i * 3 + 3; j++) {
+                arr[i].push(tabs[j]);
+            }
+        }
+        return arr;
     }
 
     const projectsGroupMapping = (arr: IProject[], index: number) => {
@@ -184,13 +119,7 @@ const OurProjects: FC = () => {
                         className={s.swiper}
                     >
                         <SwiperSlide className={s.slideWrapper}>
-                            <div className={s.slideBlock} style={{ background: "url(https://static.dw.com/image/57844523_605.jpg)" }}></div>
-                        </SwiperSlide>
-                        <SwiperSlide className={s.slideWrapper}>
-                            <div className={s.slideBlock} style={{ background: "url(https://nung.edu.ua/sites/default/files/2022-03/viber_2022-03-09_18-02-07-497.jpg)" }}></div>
-                        </SwiperSlide>
-                        <SwiperSlide className={s.slideWrapper}>
-                            <div className={s.slideBlock} style={{ background: "url(https://aam.com.ua/wp-content/uploads/56n.jpg)" }}></div>
+                            <div className={s.slideBlock} style={{ background: `url(${tabs[activeTab].imageUrl})` }}></div>
                         </SwiperSlide>
                     </Swiper>
                     <div className={s.block}>
