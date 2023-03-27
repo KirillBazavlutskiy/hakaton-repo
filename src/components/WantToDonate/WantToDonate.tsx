0ee5748f2861e1431390344e7c7e5c4291df824f
KirillBazavlutskiy <bazik345@gmail.com>
2023-03-27 16:16:45 +0300

    added payment methods

--- src/components/WantToDonate/WantToDonate.tsx
+++ src/components/WantToDonate/WantToDonate.tsx
@@ -1,8 +1,13 @@
 import s from './WantToDonate.module.scss'
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js"
 import { SectionCaption } from '../SectionCaption/SectionCaption'
+import {useState} from "react";
 
 const WantToDonate = () => {
+
+    const [paymentMethod, setPaymentMethod] = useState(1);
+
+    const [paydata, setPayData] = useState("kdfjvldjfvdfvdffdlvjkdfsdhcskdjchsjkc");
 
     return (
         <div className={s.wrapper}>
@@ -12,34 +17,119 @@ const WantToDonate = () => {
                         I want to donate
                     </SectionCaption>
                     <div className={s.buttons}>
-                        <div className={s.block}>
-                            <a className={s.way} href={"https://secure.wayforpay.com/payment/wfp"}>WayForPay</a>
-                        </div>
-                        <div className={s.block}>
-                            <PayPalScriptProvider options={{ "client-id": "ARByd3mCc6RaCPvyerFjF6UAy0W6Fszp6353DDQz3rx77DYGCApGu64n0Kr4BPM0gEoCpjDjztsfc04X" }}>
-                                <PayPalButtons />
-                            </PayPalScriptProvider>
-                        </div>
+                        {/*TODO: liqpay api & mono api*/}
+                        <button>Privat</button>
+                        <button>Mono</button>
                     </div>
                 </div>
                 <div className={s.otherWays}>
                     <SectionCaption>
                         Other ways to help
                     </SectionCaption>
-                    <ul>
-                        <li>
-                            <a href='#'>Paypal</a>
-                        </li>
-                        <li>
-                            <a href='#'>BTC</a>
-                        </li>
-                        <li>
-                            <a href='#'>ETH</a>
-                        </li>
-                        <li>
-                            <a href='#'>USDT</a>
-                        </li>
-                    </ul>
+                    <div className={s.block}>
+                        <ul>
+                            <li>
+                                <button className={paymentMethod === 1 ? s.activeBtn : s.notActiveBtn} onClick={() => setPaymentMethod(1)}>PayPal</button>
+                            </li>
+                            <li>
+                                <button className={paymentMethod === 2 ? s.activeBtn : s.notActiveBtn} onClick={() => setPaymentMethod(2)}>BTC</button>
+                            </li>
+                            <li>
+                                <button className={paymentMethod === 3 ? s.activeBtn : s.notActiveBtn} onClick={() => setPaymentMethod(3)}>ETH</button>
+                            </li>
+                            <li>
+                                <button className={paymentMethod === 4 ? s.activeBtn : s.notActiveBtn} onClick={() => setPaymentMethod(4)}>USDT</button>
+                            </li>
+                        </ul>
+                        <div className={paymentMethod === 1 ? s.payActive : s.payNotActive}>
+                            <div className={s.payBlock}>
+                                <p>{paydata}</p>
+                                <button className={s.copyBtn} onClick={() => {navigator.clipboard.writeText(paydata)}}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className={s.payBlock}>
+                                <p>{paydata}</p>
+                                <button className={s.copyBtn} onClick={() => navigator.clipboard.writeText(paydata)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                        <div className={paymentMethod === 2 ? s.payActive : s.payNotActive}>
+                            <div className={s.payBlock}>
+                                <p>{paydata}</p>
+                                <button className={s.copyBtn} onClick={() => {navigator.clipboard.writeText(paydata)}}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className={s.payBlock}>
+                                <p>{paydata}</p>
+                                <button className={s.copyBtn} onClick={() => navigator.clipboard.writeText(paydata)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                        <div className={paymentMethod === 3 ? s.payActive : s.payNotActive}>
+                            <div className={s.payBlock}>
+                                <p>{paydata}</p>
+                                <button className={s.copyBtn} onClick={() => {navigator.clipboard.writeText(paydata)}}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className={s.payBlock}>
+                                <p>{paydata}</p>
+                                <button className={s.copyBtn} onClick={() => navigator.clipboard.writeText(paydata)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                        <div className={paymentMethod === 4 ? s.payActive : s.payNotActive}>
+                            <div className={s.payBlock}>
+                                <p>{paydata}</p>
+                                <button className={s.copyBtn} onClick={() => {navigator.clipboard.writeText(paydata)}}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className={s.payBlock}>
+                                <p>{paydata}</p>
+                                <button className={s.copyBtn} onClick={() => navigator.clipboard.writeText(paydata)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
