543a12b8c5014f217cc7dbd1e3ae8a0dbcfeb060
KirillBazavlutskiy <bazik345@gmail.com>
2023-04-01 00:38:32 +0300

    added change text handler

--- src/pages/api/changeText.ts
+++ src/pages/api/changeText.ts
@@ -1,7 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { Translation } from "@/models/text";
 import fs from 'fs';
-import {TranslatedLanguage} from "@/models/text";
+import path from "path";
+import process from "process";
 
-// export const changeTextHandler = (req: NextApiRequest<TranslatedLanguage>, res: NextApiResponse) => {
-//     fs.write
-// }
+const changeTextHandler = (req: NextApiRequest, res: NextApiResponse) => {
+
+    const { method, body } = req;
+
+    switch (method) {
+        case 'PUT':
+            try {
+                const data = JSON.parse(body);
+                if (data as Translation) {
+                    const filePath = path.join(process.cwd(), 'data', 'localisation.json');
+                    fs.writeFileSync(filePath, JSON.stringify(data));
+                    res.status(200).json({ message: 'Successfully Updated!' });
+                } else {
+                    res.status(400).json({ message: 'Incorrect json!' });
+                }
+            } catch (error) {
+                console.log(error);
+                res.status(500).json({ message: 'Failed to update!' })
+            }
+            break;
+        default:
+            res.setHeader('Allow', ['PUT']);
+            res.status(405).json({ message: `Method ${method} is not allowed! Use method PUT instead!` });
+            break;
+    }
+}
+
+export default changeTextHandler;
