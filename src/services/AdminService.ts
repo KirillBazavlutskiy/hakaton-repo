832bc8f8a6e23ade488ecca42df3c660b39a2b08
KirillBazavlutskiy <bazik345@gmail.com>
2023-04-27 03:28:53 +0300

    returned get projects method

--- src/services/AdminService.ts
+++ src/services/AdminService.ts
@@ -1,9 +1,19 @@
 import {UserDTO, UserRole} from "@/models/user";
 import $api from "@/http/init";
+import {IProject} from "@/models/data";
 
 export default class AdminService {
     static fetchMembers = async (skip: number, limit: number,  role: UserRole): Promise<UserDTO[]> => {
         const { data } = await $api.get<UserDTO[]>(`/Users?skip=${skip}&limit=${limit}&role=${role}`);
         return data;
+    }
+
+    static GetProjects = async (): Promise<IProject[]> => {
+        try {
+            const { data } = await $api.get<IProject[]>('/api/OurProject');
+            return data;
+        } catch (e) {
+            return [];
+        }
     }
 }
