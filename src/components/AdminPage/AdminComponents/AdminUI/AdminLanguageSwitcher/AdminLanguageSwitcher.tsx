8ad89cf31026e59a3ed581c457e089969224ac86
KirillBazavlutskiy <bazik345@gmail.com>
2023-05-01 16:57:06 +0300

    updated styles in admin language switcher

--- src/components/AdminPage/AdminComponents/AdminUI/AdminLanguageSwitcher/AdminLanguageSwitcher.tsx
+++ src/components/AdminPage/AdminComponents/AdminUI/AdminLanguageSwitcher/AdminLanguageSwitcher.tsx
@@ -2,24 +2,32 @@ import { FC, useContext} from 'react';
 import s from './AdminLanguageSwitcher.module.scss';
 import {TranslationTextContext} from "@/pages/admin";
 import {useRouter} from "next/router";
+import Logo from '@/images/logo.png';
+import Image from "next/image";
+import cn from "classnames";
 
 const AdminLanguageSwitcher: FC = () => {
 
     const context = useContext(TranslationTextContext);
-    const { locales } = useRouter();
+    const { locales, locale } = useRouter();
 
     return (
         <div className={s.container}>
+            <div className={s.logo}>
+                <Image src={Logo} alt={'Logo'} />
+            </div>
             <div className={s.languageSwitch}>
-                <p>Перемикання між мовами</p>
                 <div className={s.languageList}>
                     {
-                        locales?.map(l => <button key={l} onClick={() => context?.setLanguage(l)}>{l}</button>)
+                        locales?.map(l =>
+                            <button
+                                key={l}
+                                className={cn(s.localeButton, context?.language === l ? s.active : s.notActive)}
+                                onClick={() => context?.setLanguage(l)}
+                            >{l.toUpperCase()}</button>)
                     }
                 </div>
-            </div>
-            <div className={s.resetState}>
-                <button onClick={() => {context?.setTranslate(context?.DefaultTranslate)}}>Відмінити зміни</button>
+                <button className={s.resetButton} onClick={() => {context?.setTranslate(context?.DefaultTranslate)}}>Відмінити зміни</button>
             </div>
         </div>
     );
