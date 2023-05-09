2812f381a2d27e86728aa86a52273c2c428079ee
KirillBazavlutskiy <bazik345@gmail.com>
2023-05-09 16:13:06 +0300

    fixed our projects

--- src/components/OurProjects/OurProjects.tsx
+++ src/components/OurProjects/OurProjects.tsx
@@ -129,8 +129,8 @@ const OurProjects: FC<IntroTextProps> = ({OurProjects, Array}) => {
                         {
                             tabs[activeTab].photos.map(p =>
                                 <SwiperSlide className={s.slideWrapper}>
-                                    {/*<div className={s.slideBlock} key={p} style={{ background: `url(https://ss.egartsites.pp.ua/${p})` }}></div>*/}
-                                    <img src={`https://ss.egartsites.pp.ua/${p}`} alt={`https://ss.egartsites.pp.ua/${p}`}/>
+                                    <div className={s.slideBlock} key={p} style={{ background: `url(https://ss.egartsites.pp.ua/${p})` }}></div>
+                                    {/*<img src={`https://ss.egartsites.pp.ua/${p}`} alt={`https://ss.egartsites.pp.ua/${p}`}/>*/}
                                 </SwiperSlide>)
                         }
                     </Swiper>
