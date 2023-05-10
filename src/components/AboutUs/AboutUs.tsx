5cf1d7721017a612c88b35216dd5a372885bcdfa
Пащенко Иван <pasenkoivan@MacBook-Pro-Pasenko.local>
2023-05-10 16:03:01 +0300

    fix about block, add lift buttons in about, fix payPlock

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -1,7 +1,8 @@
 import styles from './AboutUs.module.scss';
 import {FC} from "react";
 import {AboutUs} from "@/models/text";
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import button from "@/components/AdminPage/AdminComponents/AdminUI/Button/Button";
 
 interface AboutUsProps {
     languageProps: AboutUs;
@@ -9,32 +10,49 @@ interface AboutUsProps {
 
 const AboutUs:FC<AboutUsProps> = ({languageProps}) => {
 
+    //Variable for set active block
     const [activeBtn, setActiveBtn] = useState<number>(0)
+
+    //Array with all info
+    const [array, setArray] = useState([
+        { name: languageProps.about__title, text: languageProps.about__text },
+        { name: languageProps.mission__title, text: languageProps.mission__text },
+        { name: languageProps.value__title, text: languageProps.value__text }
+    ]);
+
+    useEffect(() => {
+        // Update the array with new language data
+        setArray([
+            { name: languageProps.about__title, text: languageProps.about__text },
+            { name: languageProps.mission__title, text: languageProps.mission__text },
+            { name: languageProps.value__title, text: languageProps.value__text }
+        ]);
+    }, [languageProps]); // Add languageProps as a dependency
+
     return (
         <div className={styles.wrapper}>
-            <button onClick={() => activeBtn === 0 ? setActiveBtn(1) : setActiveBtn(0)}>
-                {activeBtn === 1 ? "Close block" : "Open block"}
-            </button>
-            <div className={activeBtn === 1 ? styles.container : styles.shamil}>
-                <div className={styles.aboutUs}>
-                    <h1>{languageProps.about__title}</h1>
-                    <p>
-                        {languageProps.about__text}
-                    </p>
-                </div>
-                <div className={styles.mission}>
-                    <h1>{languageProps.mission__title}</h1>
-                    <p>
-                        {languageProps.mission__text}
-                    </p>
-                </div>
-                <div className={styles.value}>
-                    <h1>{languageProps.value__title}</h1>
-                    <p>
-                        {languageProps.value__text}
-                    </p>
-                </div>
-                <div className={styles.emptyBlock}></div>
+            <div className={styles.btnList}>
+                {
+                    array.map((res:any, index:number) =>
+                        <button
+                            className={activeBtn === index ? styles.activeBtn : styles.notActiveBtn}
+                            onClick={() => setActiveBtn(index)}>
+                            {res.name}
+                        </button>
+                    )
+                }
+            </div>
+            <div className={styles.container}>
+                {
+                    array.map((res:any, index:number) =>
+                        <div className={activeBtn === index ? styles.block : styles.shamil}>
+                            <h1>{res.name}</h1>
+                            <p>
+                                {res.text}
+                            </p>
+                        </div>
+                    )
+                }
             </div>
         </div>
     );
